23f603d7e1fce4703729c121c3419a587cfc956b
aariya50 <amarj003@umn.edu>
2026-02-13 14:38:40 -0500

    Bump openclaw-toggle to v3.0.0

--- Casks/openclaw-toggle.rb
+++ Casks/openclaw-toggle.rb
@@ -1,5 +1,5 @@
 cask "openclaw-toggle" do
-  version "2.0.2"
+  version "3.0.0"
   sha256 :no_check
 
   url "https://github.com/aariya50/openclaw-toggle/releases/download/v#{version}/OpenClawToggle.app.zip"
